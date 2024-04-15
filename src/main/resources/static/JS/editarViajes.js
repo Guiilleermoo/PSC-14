e7069ecfc787382b1a0a284584f68df209057069
unai-aira <unai.aira@opendeusto.es>
2024-04-15 15:49:30 +0200

    Implementación de botón y función para reordenar la tabla viajes

--- src/main/resources/static/JS/editarViajes.js
+++ src/main/resources/static/JS/editarViajes.js
@@ -36,6 +36,43 @@ let listarViajes = async () => {
     }
     document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
 }
+
+let listarViajesOrdenados = async () => {
+    const peticion = await fetch('http://localhost:8080/sql/viajes', {
+
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const viajes = await peticion.json();
+    viajes.sort((a, b) => b.duracion - a.duracion);
+
+    let contenidoTabla = "";
+    for (let viaje of viajes) {
+        let contenidoFila = `<tr>
+            <td>${viaje.id}</td>
+            <td>${viaje.empresa}</td>
+            <td>${viaje.origen}</td>
+            <td>${viaje.destino}</td>
+            <td>${viaje.fecha}</td>
+            <td>${viaje.duracion}</td>
+            <td>${viaje.precio}</td>
+            <td>${viaje.asientosTotales}</td>
+            <td>${viaje.asientosDisponibles}</td>
+            <td>${viaje.oferta}</td>
+            <td>
+            <i onClick="editarViaje(${viaje.id})"class="material-icons button edit">edit</i>
+            <i onClick="borrarViaje(${viaje.id})"class="material-icons button delete">delete</i>
+            </td>
+        </tr>
+        ` 
+        contenidoTabla += contenidoFila;
+    }
+    document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
+}
+
 let borrarViaje = async (id) => {
     const peticion = await fetch('http://localhost:8080/sql/eliminarViaje', {
 
--- src/main/resources/static/JS/reservarViaje.js
+++ src/main/resources/static/JS/reservarViaje.js
@@ -35,6 +35,94 @@ let listarViajes = async () => {
     }
     document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
 }
+
+let listarViajesOrdenados = async () => {
+    const peticion = await fetch('http://localhost:8080/sql/viajes', {
+
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const viajes = await peticion.json();
+    viajes.sort((a, b) => a.duracion - b.duracion);
+
+    let contenidoTabla = "";
+    for (let viaje of viajes) {
+        let contenidoFila = `<tr>
+            <td>${viaje.id}</td>
+            <td>${viaje.empresa}</td>
+            <td>${viaje.origen}</td>
+            <td>${viaje.destino}</td>
+            <td>${viaje.fecha}</td>
+            <td>${viaje.duracion}</td>
+            <td>${viaje.precio}</td>
+            <td>${viaje.asientosTotales}</td>
+            <td>${viaje.asientosDisponibles}</td>
+            <td>${viaje.oferta}</td>
+            <td>
+            <i onClick="editarViaje(${viaje.id})"class="material-icons button edit">edit</i>
+            <i onClick="borrarViaje(${viaje.id})"class="material-icons button delete">delete</i>
+            </td>
+        </tr>
+        ` 
+        contenidoTabla += contenidoFila;
+    }
+    document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
+}
+
+let listarViajesOrdenados2 = async () => {
+
+// Paso 1: Obtener los datos de la tabla HTML
+let tabla = document.getElementById("tabla");
+let filas = tabla.getElementsByTagName("tr");
+let datos = [];
+
+for (let i = 2; i < filas.length; i++) { // Empezamos desde 2 para omitir la fila de encabezado
+    let fila = filas[i];
+    let celdas = fila.getElementsByTagName("td");
+    let id = parseInt(celdas[0].innerText);
+    let empresa = celdas[1].innerText;
+    let origen = celdas[2].innerText;
+    let destino = celdas[3].innerText;
+    let fecha = celdas[4].innerText;
+    let duracion = parseInt(celdas[5].innerText);
+    let precio = parseFloat(celdas[6].innerText);
+    let asientosTotales = parseInt(celdas[7].innerText);
+    let asientosDisponibles = parseInt(celdas[8].innerText);
+    let oferta = celdas[9].innerText;
+    datos.push({ id: id, empresa: empresa, origen: origen, destino: destino, fecha: fecha, duracion: duracion, precio: precio, asientosTotales: asientosTotales, asientosDisponibles: asientosDisponibles, oferta: oferta});
+}
+
+// Paso 2: Ordenar los datos por edad
+datos.sort((a, b) => a.duracion - b.duracion);
+
+// Paso 3: Actualizar la tabla HTML con los datos ordenados
+let contenidoTabla = "";
+    for (let viaje of datos) {
+        let contenidoFila = `<tr>
+            <td>${viaje.id}</td>
+            <td>${viaje.empresa}</td>
+            <td>${viaje.origen}</td>
+            <td>${viaje.destino}</td>
+            <td>${viaje.fecha}</td>
+            <td>${viaje.duracion}</td>
+            <td>${viaje.precio}</td>
+            <td>${viaje.asientosTotales}</td>
+            <td>${viaje.asientosDisponibles}</td>
+            <td>${viaje.oferta}</td>
+            <td>
+            <i onClick="editarViaje(${viaje.id})"class="material-icons button edit">edit</i>
+            <i onClick="borrarViaje(${viaje.id})"class="material-icons button delete">delete</i>
+            </td>
+        </tr>
+        ` 
+        contenidoTabla += contenidoFila;
+    }
+    document.querySelector("#tabla tbody").outerHTML = contenidoTabla;
+}
+
 let idEditar;
 
 let reservarViaje = async (id) => {
