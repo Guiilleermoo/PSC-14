f8bbd23ce944f81958bbcda26dbfd4073c3c6bb3
unai-aira <unai.aira@opendeusto.es>
2024-05-21 19:01:47 +0200

    Implementación correcta del MySQL

--- src/main/java/es/config/dbConfig.java
+++ src/main/java/es/config/dbConfig.java
@@ -15,7 +15,7 @@ public class dbConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/deustotraveladvaisor");
+        dataSource.setUrl("jdbc:mysql://localhost/deustotraveladvisor");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
         return dataSource;
